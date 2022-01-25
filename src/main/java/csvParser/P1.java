9a5df9e9d6de420bc8dccc088c25fccca7b40044
JoseE. Aguilar Escamilla <aguilar.jose11@outlook.com>
2022-01-25 12:18:09 -0600

    Basic Testing

--- src/main/java/csvParser/P1.java
+++ src/main/java/csvParser/P1.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 
 public class P1 {
 
-	public static void printCSV(String stringEN, String stringES) {
-		System.out.println(stringEN + "," + stringES);
+	public static String printCSV(BufferedReader br) {
+		String output = "";
+		String line;
+		try {
+		while ((line = br.readLine()) != null) { // read each line of csv
+			// query each line at a time
+			String[] aLine = line.split(",");
+			String stringEN = aLine[0];
+			String stringES = aLine[1];
+
+			output = output.concat(stringEN + "," + stringES + "\n");
+		}
+		}
+		catch(Exception e){
+			return "";
+		}
+		return output;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -21,14 +36,8 @@ public class P1 {
 		String correctAnswer = "A,A\nB,B\nC,C\n,CH\nD,D\nE,E\nF,F\nG,G\nH,H\nI,I\nJ,J\nK,K\nL,L\n,LL\nM,M\nN,N\n,Ñ\nO,O\nP,P\nQ,Q\nR,R\n,RR\nS,S\nT,T\nU,U\nV,V\nW,W\nX,X\nY,Y\nZ,Z";
 
 		System.out.println(correctAnswer);
-		while ((line = br.readLine()) != null) { // read each line of csv
-			// query each line at a time
-			String[] aLine = line.split(",");
-			String stringEN = aLine[0];
-			String stringES = aLine[1];
-
-			printCSV(stringEN, stringES);
-		}
+		System.out.println(printCSV(br));
+		
 		br.close();
 		sc.close();
 	}
--- src/test/java/JUnit5ExampleTest.java
+++ src/test/java/JUnit5ExampleTest.java
@@ -1,9 +1,26 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+
  
 class JUnit5ExampleTest {
  
     @Test
     void justAnExample() {
-        System.out.println("This test method should be run");
+
+        BufferedReader br;
+        try{
+            br = new BufferedReader(new FileReader("src/test/resources/csvFile.csv"));
+        }
+        catch(Exception e){
+            assertTrue(false);
+            return;
+        }
+        P1.printCSV(br);
+        assertTrue(true);
     }
 }
